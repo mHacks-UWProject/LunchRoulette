d8528e05da4c45913c8d00fba9c56d1903aa9cf4
Valentin Tsatskin <val@valtsatskin.com>
2013-09-21 18:37:19 -0400

    initial

---
+++ LunchRoulette/LunchRoulette/LRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LRAppDelegate.h
+//  LunchRoulette
+//
+//  Created by Valentin Tsatskin on 2013-09-21.
+//  Copyright (c) 2013 Valentin Tsatskin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LunchRoulette/LunchRoulette/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LunchRoulette
+//
+//  Created by Valentin Tsatskin on 2013-09-21.
+//  Copyright (c) 2013 Valentin Tsatskin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LRAppDelegate class]));
+    }
+}
