b2a6228ff3550a03b6a915f5180333e5cdae3224
VicV <victorvuc@gmail.com>
2013-09-21 19:09:13 -0400

    Android beginnings. The origin. The great.

---
+++ LunchRoulette-Android/gen/com/mhacks/lunchroulette/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mhacks.lunchroulette;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ LunchRoulette-Android/src/com/mhacks/lunchroulette/ConnectActivity.java
@@ -0,0 +1,15 @@
+package com.mhacks.lunchroulette;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ConnectActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_connect);
+	}
+
+}
